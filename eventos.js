f2ff8d934a548d33cfa0565847b45a7461d6fc12
narce2898 <natyarce2014@gmail.com>
2023-08-14 17:33:02 -0300

    Solución al problema de clic

--- eventos.js
+++ eventos.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const divContainer = document.querySelector(".boton-container");
 
-    divContainer.addEventListener("click", function() {
+    divContainer.addEventListener("click", function(event) {
         alert("Hola! Soy el div");
+        event.stopPropagation(); // Detiene la propagación del evento click
+    });
+
+    const button = document.querySelector(".boton");
+    
+    button.addEventListener("click", function(event) {
+        alert("Hola! Soy el botón");
+        event.stopPropagation(); // Detiene la propagación del evento click
     });
 });
