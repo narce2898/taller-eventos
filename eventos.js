bcc6a9ec2984e94175d4c583a0967717f4d6490c
narce2898 <natyarce2014@gmail.com>
2023-08-14 14:00:19 -0300

    Agregado manejador de eventos al div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const divContainer = document.querySelector(".boton-container");
+
+    divContainer.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
